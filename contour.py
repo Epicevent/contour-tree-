48125bf0f4aa1a29f9ffc7804a4e15ff7f1b6129
Geunwoo Nam <alorse45@gmail.com>
2019-03-11 14:50:44 +0900

    I used PIL library to load and convert the image to a  numpy array. Still working on other procedures.

--- contour.py
+++ contour.py
@@ -38,5 +38,45 @@
     3 7 11 15 19
 """
 
-def image_open(filedir):
-    pass
+# libraries
+from PIL import Image
+import numpy as np
+
+"""
+    [index, value]
+    ex. [[0, 1], [1, 255], [2, 200], ...]
+"""
+
+def image_open(_imgFile):
+    """
+    Read the img and return PIL Image object
+    """
+    # Read the image file
+    img = Image.open(_imgFile)
+    return img
+
+def image_get_width(_img):
+    """
+    Input: PIL.Image _img
+    Output: int imgWidth
+    """
+    imgWidth = _img.width
+    return imgWidth
+
+def image_get_height(_img):
+    """
+    Input: PIL.Image _img
+    Output: int imgHeight
+    """
+    imgHeight = _img.height
+    return imgHeight
+
+def image_to_numpy_array(_img):
+    """
+    Input: PIL.Image _img
+    Description: conver the PIL image into a grey scale image(8-bit pixels, black-and-white)
+    Output: numpy array
+    """
+    img_greyscale = _img.convert('L')
+    npImgArr = np.array(img_greyscale)
+    return npImgArr
